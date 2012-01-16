15808fe0d851d8acfc4406185f5903d1bbbda31c
marco.molinari1980 <marco.molinari1980@gmail.com>
2012-01-16 22:30:42 +0000

    prova punte

---
+++ src/view/oggetti/punte/PainterPuntaBase.java
@@ -0,0 +1,12 @@
+package view.oggetti.punte;
+
+import view.oggetti.IFormaGeometrica;
+import view.oggetti.painter.PainterBase;
+
+public class PainterPuntaBase extends PainterBase{
+
+	public PainterPuntaBase(IFormaGeometrica oggettoGrafico) {
+		super(oggettoGrafico);
+	}
+
+}
---
+++ src/view/oggetti/punte/PuntaV.java
@@ -0,0 +1,5 @@
+package view.oggetti.punte;
+
+public class PuntaV {
+
+}
