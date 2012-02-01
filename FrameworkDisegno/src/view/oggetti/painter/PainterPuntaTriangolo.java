3a9ef994731f519cbf4f84883b48afb191b7a1b8
marco.molinari1980@gmail.com <marco.molinari1980@gmail.com@8be8bb78-a580-ef64-62f8-33edb657bb8e>
2012-02-01 22:28:47 +0000

    corretta punta

--- FrameworkDisegno/src/view/oggetti/painter/PainterPuntaTriangolo.java
+++ FrameworkDisegno/src/view/oggetti/painter/PainterPuntaTriangolo.java
@@ -19,11 +19,15 @@ public class PainterPuntaTriangolo extends PainterPuntaBase {
 		super.paint(g);
 		PuntaTriangolo puntaTriangolo = (PuntaTriangolo) getOggettoGrafico();
 		GeneralPath path = new GeneralPath();
-		path.lineTo(puntaTriangolo.getLatoDestro().getDestinazione().getX(), puntaTriangolo.getLatoDestro().getDestinazione().getY());
+		path.moveTo(puntaTriangolo.getLatoDestro().getDestinazione().getX(), puntaTriangolo.getLatoDestro().getDestinazione().getY());
 		path.lineTo(puntaTriangolo.getLatoSinistro().getDestinazione().getX(), puntaTriangolo.getLatoSinistro().getDestinazione().getY());
 		path.lineTo(puntaTriangolo.getEstremoCentrale().getX(), puntaTriangolo.getEstremoCentrale().getY());
 		Graphics2D g2 = (Graphics2D) g;
+		path.closePath();
 		g2.setColor(Color.WHITE);
 		g2.fill(path);
+
+		g2.setColor(Color.BLACK);
+		g2.draw(path);
 	}
 }
